6717e9c61d556e466681a056e38bfbb8342956fb
Ekrem ÖZBAYRAM <36367222+ozbayramekrem@users.noreply.github.com>
2018-02-15 19:29:37 +0300

    Proje Ekleme

---
+++ HesapMakinesi/HesapMakinesi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string dvm = "d";
+            while (dvm == "d")
+            {
+                Console.Write("İlk Değer: ");
+                double deger1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Yapmak İstediğiniz İşlem: ");
+                string islem = Console.ReadLine();
+                Console.Write("İkici Değer: ");
+                double deger2 = Convert.ToDouble(Console.ReadLine());
+                if (islem == "+")
+                {
+                    İslemler islm = new İslemler();
+                    Console.WriteLine(islm.toplama(deger1, deger2));
+                }
+                else
+                {
+                    Console.WriteLine("İslem Hatalı..");
+                }
+                Console.WriteLine("\nDevam Etmek İçin ' d ' Harfine Basınız..");
+                dvm = Console.ReadLine();
+                if(dvm != "d")
+                {
+                    Console.WriteLine("\nGüle Güle");
+                    break;
+                }
+                Console.WriteLine("--------------------------------------------");
+            }
+        }
+    }
+}
---
+++ HesapMakinesi/HesapMakinesi/İslemler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class İslemler
+    {
+        public double toplama(double a, double b)
+        {
+            double sonuc= a+b;
+            return sonuc;
+        }
+    }
+}
